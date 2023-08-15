7081aa6ceab1029a712650f5c489678a7a564050
CaptainPicarl <109193553+CaptainPicarl@users.noreply.github.com>
2023-08-15 11:38:50 -0400

    Updated utilities

--- Utility.cs
+++ Utility.cs
@@ -308,9 +308,6 @@ namespace IngameScript
 
             foreach (IMyTerminalBlock turret in TURRETS)
             {
-                //Echo($"{turret.Name}\n");
-                //Echo($"{turret.BlockDefinition.TypeId}\n");
-                //Echo($"{turret.BlockDefinition.SubtypeName}\n");
                 WCapi.GetBlockWeaponMap(turret, TEMP_BWMAP);
 
                 foreach (string turretKey in TEMP_BWMAP.Keys)
@@ -341,75 +338,6 @@ namespace IngameScript
                 api.ToggleWeaponFire(turret, true, true, 0);
             }
         }
-
-        /*        public void FireOnceAllWeaponcoreWeapons(WcPbApi api)
-                {
-                    TURRETS = new List<IMyTerminalBlock>();
-                    TEMP_TUR = new List<MyDefinitionId>();
-
-                    api.GetAllCoreWeapons(TEMP_TUR);
-
-                    List<string> definitionSubIds = new List<string>();
-
-                    TEMP_TUR.ForEach(d => definitionSubIds.Add(d.SubtypeName));
-
-                    GridTerminalSystem.GetBlocksOfType<IMyTerminalBlock>(TURRETS, b => b.CubeGrid ==
-                        Me.CubeGrid && definitionSubIds.Contains(b.BlockDefinition.SubtypeName));
-
-                    Echo("Firing Weaponcore Turrets\n");
-
-                    foreach (IMyTerminalBlock weapon in TURRETS)
-                    {
-                        api.ToggleWeaponFire(weapon, true, true, 0);
-                    }
-                }*/
-
-
-        /*       public void RenderShipCockpitLCDs(List<IMyCockpit> ShipCockpits)
-               {
-
-                   // Cockpit configuration (Has to be done after alert draw)
-                   foreach (IMyCockpit cockpit in ShipCockpits)
-                   {
-                       // due to language constrains with switches - had to do this funky.
-
-
-                       surfaceCount = cockpit.SurfaceCount;
-
-                       if (surfaceCount > 0)
-                       {
-                           mainScreen = cockpit.GetSurface(0);
-                           mainScreen.ContentType = ContentType.SCRIPT;
-                           mainScreen.Script = "TSS_ArtificialHorizon";
-                           mainScreen.ScriptBackgroundColor = Color.Black;
-                           mainScreen.ScriptForegroundColor = Color.DarkOrange;
-                       }
-
-                       if (surfaceCount > 1)
-                       {
-                           auxScreen1 = cockpit.GetSurface(1);
-                           auxScreen1.ContentType = ContentType.SCRIPT;
-                           auxScreen1.Script = "TSS_Velocity";
-                           auxScreen1.ScriptBackgroundColor = Color.Black;
-                           auxScreen1.ScriptForegroundColor = Color.DarkOrange;
-                       }
-
-                       if (surfaceCount > 2)
-                       {
-                           auxScreen2 = cockpit.GetSurface(2);
-                           auxScreen2.ContentType = ContentType.SCRIPT;
-                           auxScreen2.Script = "TSS_EnergyHydrogen";
-                           auxScreen2.ScriptBackgroundColor = Color.Black;
-                           auxScreen2.ScriptForegroundColor = Color.DarkOrange;
-                       }
-
-                       if (surfaceCount > 3)
-                       {
-                           auxScreen3 = cockpit.GetSurface(3);
-                           RenderShieldPercentageScreen(auxScreen3, false);
-                       }
-                   }
-               } */
 
         public void EchoTerminalProperties(IMyTerminalBlock block)
         {
@@ -461,109 +389,7 @@ namespace IngameScript
                 Echo(contact.Name.ToString());
                 Echo(contact.Position.ToString());
                 Echo(contact.Velocity.ToString());
-
-            }
-
-        }
-        /*
-        public void doAutopilotTravelRC(Vector3 dest, string destName,float SpeedLimitVar)
-        {
-            foreach(IMyRemoteControl rc in ShipRemoteControlBlocks)
-            {
-                rc.FlightMode = FlightMode.OneWay;
-                rc.DampenersOverride = true;
-                rc.ControlThrusters = true;
-                rc.HandBrake = false;
-                rc.SpeedLimit = SpeedLimitVar;
-                rc.AddWaypoint(dest,destName);
-                rc.SetAutoPilotEnabled(true);
             }
         }
-
-        public void travelToZero()
-        {
-            Vector3 currPosition = Me.Position;
-            Echo(currPosition.ToString());
-        }
-
-        public void travelToGPS(Vector3 dest, List<IMyThrust> thrusters)
-        {
-            Vector3 CurrPosition = Me.GetPosition();
-
-            foreach (IMyThrust thrust in thrusters)
-            {
-
-            }
-        }
-
-        //TODO: Finish this! BROKEN! DO NOT USE!
-        public Dictionary<int, List<IMyThrust>> getThrustersViaAxisDict(List<IMyThrust> thrusters, IMyRemoteControl rc)
-        {
-            Dictionary<int, List<IMyThrust>> result = new Dictionary<int, List<IMyThrust>>();
-
-            List<IMyThrust> fwdThrustList = new List<IMyThrust>();
-            List<IMyThrust> aftThrustList = new List<IMyThrust>();
-            List<IMyThrust> leftThrustList = new List<IMyThrust>();
-            List<IMyThrust> rightThrustList = new List<IMyThrust>();
-            List<IMyThrust> upThrustList = new List<IMyThrust>();
-            List<IMyThrust> downThrustList = new List<IMyThrust>();
-
-            foreach (IMyThrust thrust in thrusters)
-            {
-                // collects forward thrusters
-                if (thrust.Orientation.Forward == Me.Orientation.Forward)
-                {
-                    fwdThrustList.Add(thrust);
-                }
-
-                if (thrust.Orientation.Forward == Me.Orientation.Left)
-                {
-                    leftThrustList.Add(thrust);
-                }
-            }
-            return null;
-        }
-
-        // utility for Sanitizing
-        public void SanitizeAllCustomData(List<IMyTerminalBlock> allBlocksGroup)
-        {
-            foreach (IMyTerminalBlock block in allBlocksGroup)
-            {
-                block.CustomData = null;
-            }
-
-        }
-
-        public void ScriptSelfDestructAllPBs(List<IMyProgrammableBlock> pbs)
-        {
-            foreach (IMyProgrammableBlock block in pbs)
-            {
-                for (int i = 0; i < block.SurfaceCount; i++)
-                {
-                    IMyTextSurface screen = block.GetSurface(i);
-                    screen.ContentType = ContentType.TEXT_AND_IMAGE;
-                    screen.WriteText("NO JD'S REMAINING!\nABANDON SHIP!");
-                    block.CustomData = "";
-                    // TODO: Still no clue how to actually delete our own script...
-
-
-                }
-            }
-
-            // Figure that ^ out. The below stuff just cleans up...whatever might be around. I'm grasping for completion here.
-            Me.CustomData = "";
-            Me.CustomName = "";
-        }
-
-        public Vector3 getTargetPos(Vector3 targetPos, Vector3 lastTargetPos) { 
-            Vector3 error = targetPos - lastTargetPos;
-            return targetPos + error;
-        }
-
-        public void CWCWepsGUNSTRIP(List<IMyTerminalBlock> turretsList)
-        {
-
-        }
-        */
     }
 }
